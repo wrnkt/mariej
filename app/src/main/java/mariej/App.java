b4ed4587814488378b4d683b381c14fdf348f6fc
Niko Ware <wrnkt1@gmail.com>
2023-09-30 22:03:10 -0400

    Test building instruction set

--- app/src/main/java/mariej/App.java
+++ app/src/main/java/mariej/App.java
@@ -1,11 +1,14 @@
 package mariej;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import mariej.AC;
+import mariej.InstructionSetBuilder;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+        var isb = new InstructionSetBuilder();
+        isb.generateSet();
     }
 }
